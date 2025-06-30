32446d6cdd76f6c9d22b6a457c0e4096e112ff20
bp.shirai <bp.shirai@gmail.com>
2025-06-30 23:03:04 +0900

    Add First files

---
+++ Source/Bomberman/Private/World/Powerup.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World/Powerup.h"
+
+// Sets default values
+APowerup::APowerup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APowerup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APowerup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Bomberman/Public/Core/GameplayLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameplayLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_API UGameplayLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
