cb9df73f9dcdaf045cf8543665700886a983dc6e
bp.shirai <bp.shirai@gmail.com>
2025-06-30 22:55:28 +0900

    first commit

---
+++ Source/Bomberman/Private/Player/BombermanCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/BombermanCharacter.h"
+
+// Sets default values
+ABombermanCharacter::ABombermanCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABombermanCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABombermanCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABombermanCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Bomberman/Private/Player/FollowCamera.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/FollowCamera.h"
+
+// Sets default values
+AFollowCamera::AFollowCamera()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFollowCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFollowCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
