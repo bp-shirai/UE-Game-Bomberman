5d28fac01aa7ae39ca4e49bad201bd82875e2693
bp.shirai <bp.shirai@gmail.com>
2025-07-06 11:16:13 +0900

    Update .gitignore

--- GenerateProjectFiles.bat
+++
@@ -1,3 +0,0 @@
-cd %~d0%~p0
-setlocal
-powershell -command "$uproject = Get-ChildItem "*.uproject" -Name; $bin = & { (Get-ItemProperty 'Registry::HKEY_CLASSES_ROOT\Unreal.ProjectFile\shell\rungenproj' -Name 'Icon' ).'Icon' }; $bin + ' -projectfiles %cd%\' + $uproject" | cmd.exe
